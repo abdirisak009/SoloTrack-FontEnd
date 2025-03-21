c666a486d8e9aa391ce64a8ba5ecafb7369d7b67
amiin <amiinali29@gmail.com>
2025-03-21 16:09:46 +0300

    added page and 2-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-const App = () => {
-  return <div></div>;
-};
-=======
 import React from 'react'
 import WeightLoss from './pages/WeightLoss'
 
@@ -16,6 +9,5 @@ const App = () => {
     </div>
   )
 }
->>>>>>> 287359e (added WEIGHTLOSS page and added CARD,FOODMEAL,SUCCESSSTORIES components and FOODMEAL images)
 
 export default App;
