01ad05a19371c43c242c3c439bfd8c8e9146deb7
mohamed arp <mhamedabdi3801@gmail.com>
2025-03-21 07:08:45 -0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react'
+import Banner from "../assets/mages/banner (1).jpg"
 
-const Hero = () => {
-  return <div>This Hero Section</div>;
-};
+export const Hero = () => {
+  return (
+    <div className='pt-6  overflow-hidden'>
+<h1 className='text-[40px] font-bold text-black text-center  relative pt-6 z-10'>
+  Lorem ipsum dolor amet,
+  <br></br>repellendus tempora.
+</h1> 
+<img className='mt-7 md-flex w-3vh  absolute' src={Banner} alt="" />
 
-export default Hero;
+
+
+
+    </div>
+  )
+}
+
+export default Hero
