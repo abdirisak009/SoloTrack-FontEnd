6565b7a219c1c1757166398934564aa46cd2477d
omar <omar@gmail.com>
2025-03-21 15:33:26 +0300

    jsadklfj

--- src/components/Card.jsx
+++
@@ -1,8 +0,0 @@
-export function Card({ children, className }) {
-    return <div className={`p-4 rounded-lg shadow-md bg-white ${className}`}>{children}</div>;
-  }
-  
-  export function CardContent({ children }) {
-    return <div className="p-2">{children}</div>;
-  }
-  
