15bf73d155b2a7f3421e1c8608a07f19ebd85137
iqra <iqra@gmail.com>
2025-03-26 23:14:50 +0300

    waxaa ku soo daray header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
+import React from 'react'
+import logo from '../assets/images/logo.png'
 const Header = () => {
-  return <div>This Header Section</div>;
-};
+  return (
+    <>
+       <div className="bg-[#D8EFE3] rounded-full p-4 flex items-center justify-between w-full mt-10 px-20 py-8 text-2xl">
+      {/* Logo Section */}
+      <img src={logo} className="w-[170px]"/>
 
-export default Header;
+      {/* Navigation Links */}
+      <nav className="flex space-x-6 text-black font-normal ">
+        <a href="#" className="hover:text-green-600">Weight Loss</a>
+        <a href="#" className="hover:text-green-600">Nutrition & Diet</a>
+        <a href="#" className="hover:text-green-600">Recipes & Cooking</a>
+        <a href="#" className="hover:text-green-600">Lifestyle & Mindset</a>
+      </nav>
+
+      {/* Sign In */}
+      <a href="#" className="font-bold hover:text-green-600">SignIn</a>
+    </div>
+  ;
+
+    </>
+  )
+}
+
+export default Header
