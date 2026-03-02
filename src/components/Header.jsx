77ee6a66dce3ee99cb1477e25af337c956a01d3c
sadaqsam <sadaqsamor99@gmail.com>
2026-03-02 13:50:18 +0300

    fixed marging nd padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,38 +12,38 @@ const Header = () => {
 
   return (
     <>
-      <div className="bg-[#D8EFE3] rounded-full p-4 flex items-center justify-between w-full mt-10 px-4 md:px-12 py-6 text-xl md:text-2xl">
+      <div className="bg-[#D8EFE3] rounded p-4 flex items-center justify-between w-full  px-4 md:px-12 py-3 mb-8 font-bold text-xl foont-bold cursor-pointer">
         {/* Logo Section */}
         <Link to="/" className="hover:text-green-600">
           <img src={logo} className="w-[120px] md:w-[170px]" alt="Logo" />
         </Link>
 
         {/* Navigation Links (Desktop) */}
-        <ul className="hidden lg:flex space-x-6 text-black font-normal">
+        <ul className="hidden lg:flex space-x-6 text-black">
           <li>
-            <Link to="/WeightLoss" className="hover:text-green-600">
+            <Link to="/WeightLoss" className="hover:text-green-800">
               Weight Loss
             </Link>
           </li>
           <li>
-            <Link to="/Nutrition" className="hover:text-green-600">
+            <Link to="/Nutrition" className="hover:text-green-800">
               Nutrition & Diet
             </Link>
           </li>
           <li>
-            <Link to="/RecipeCooking" className="hover:text-green-600">
+            <Link to="/RecipeCooking" className="hover:text-green-800">
               Recipes & Cooking
             </Link>
           </li>
           <li>
-            <Link to="/LifestyleMindset" className="hover:text-green-600">
+            <Link to="/LifestyleMindset" className="hover:text-green-800">
               Lifestyle & Mindset
             </Link>
           </li>
         </ul>
 
         {/* Sign In (Desktop) */}
-        <Link to="/Signup" className="hidden lg:block font-bold hover:text-green-600">
+        <Link to="/Signup" className="hidden lg:block font-bold hover:text-green-800">
           SignIn
         </Link>
 
